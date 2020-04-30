64999124611b8c2608b56614a2f23b52031611bb
Steve Carter <steve.carter@etas.com>
2020-04-30 12:12:09 +0100

    First test, shows up msys bug

--- test_bash_environment.sh
+++ test_bash_environment.sh
@@ -1,8 +1,25 @@
-#/bin/bash
+#!/bin/bash
 
 function start_test {
-    printf -- '================================\n%s\n' "$1"
+    printf -- '================================\n%s\n' "$*"
 }
 
-start_test hello
+function check {
+    printf -- '--------------------------------\ncheck: %s\n' "$*"
+    if "$@"
+    then
+        printf -- '-- OK --------------------------\n'
+    else
+        printf -- '-- FAIL ------------------------\n'
+    fi
+}
+
+
+TMP_FLD=$(mktemp -d _runXXXXX)
+
+start_test "loop status check"
+
+t/t1.sh > $TMP_FLD/t1.out
+
+check diff t/t1.expected $TMP_FLD/t1.out 
 
