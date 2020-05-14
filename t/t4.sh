b7d7b757261c7fa1e014dd9ff579835223592315
Steve Carter <sweavo@gmail.com>
2020-05-14 16:00:07 +0100

    t4 can you do clip /?

---
+++ t/t4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+## early WSL could not execute windows programs, at least that's what I heard.
+## meanwhile, MSys interprets /? to be a path and attempts to convert it to a 
+## windows path.
+
+function exit_handler 
+{
+    echo exit_status $?
+    echo done
+}
+trap exit_handler EXIT
+
+set -e # exit on error
+
+/mnt/c/windows/System32/clip.exe /?
+
